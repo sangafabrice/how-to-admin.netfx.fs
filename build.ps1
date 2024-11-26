c20a83fc32de3a4cd625741f27d5840169a1d8b5
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-11-26 06:45:59 +0100

    refactor: Use individual arguments and Management classes

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-<#PSScriptInfo .VERSION 1.0.3#>
+<#PSScriptInfo .VERSION 1.0.4#>
 
 using namespace System.Management.Automation
 [CmdletBinding()]
@@ -29,14 +29,14 @@ param ()
 
   function ImportMgmtClass([string] $ClassName) {
     $FileName = $ClassName.Replace('_', '.')
-    fsc.exe /nologo /warn:0 /target:library /out:$(($ClassDll = "$BinDir\$FileName.dll")) /reference:"$BinDir\Interop.WbemScripting.dll" $AssemblyInfoFs "$SrcDir\$FileName.fs"
+    fsc.exe /nologo /warn:0 /target:library /out:$(($ClassDll = "$BinDir\$FileName.dll")) /reference:System.Management.dll $AssemblyInfoFs "$SrcDir\$FileName.fs"
     return $ClassDll
   }
 
   # Compile the source code with fsc.exe.
   $EnvPath = $Env:Path
   $Env:Path = "$Env:ProgramFiles\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\FSharp\Tools\;$Env:Path"
-  fsc.exe /nologo /target:$($DebugPreference -eq 'Continue' ? 'exe':'winexe') /win32icon:"$PSScriptRoot\menu.ico" /reference:$(ImportMgmtClass StdRegProv) /reference:"$BinDir\Interop.WbemScripting.dll" /reference:"$BinDir\Interop.IWshRuntimeLibrary.dll" /reference:$(Get-WpfLibrary PresentationFramework) /reference:$(Get-WpfLibrary PresentationCore) /reference:$(Get-WpfLibrary WindowsBase) /reference:System.Xaml.dll /out:$(($ConvertExe = "$BinDir\cvmd2html.exe")) $AssemblyInfoFs "$SrcDir\Util.fs" "$SrcDir\Parameters.fs" "$SrcDir\Package.fs" "$SrcDir\Setup.fs" "$SrcDir\ErrorLog.fs" "$PSScriptRoot\Program.fs"
+  fsc.exe /nologo /target:$($DebugPreference -eq 'Continue' ? 'exe':'winexe') /win32icon:"$PSScriptRoot\menu.ico" /reference:$(ImportMgmtClass StdRegProv) /reference:System.Management.dll /reference:"$BinDir\Interop.IWshRuntimeLibrary.dll" /reference:$(Get-WpfLibrary PresentationFramework) /reference:$(Get-WpfLibrary PresentationCore) /reference:$(Get-WpfLibrary WindowsBase) /reference:System.Xaml.dll /out:$(($ConvertExe = "$BinDir\cvmd2html.exe")) $AssemblyInfoFs "$SrcDir\Util.fs" "$SrcDir\Parameters.fs" "$SrcDir\Package.fs" "$SrcDir\Setup.fs" "$SrcDir\ErrorLog.fs" "$PSScriptRoot\Program.fs"
   $Env:Path = $EnvPath
 
   if ($LASTEXITCODE -eq 0) {
--- src/StdRegProv.fs
+++ src/StdRegProv.fs
@@ -1,13 +1,12 @@
 /// <summary>StdRegProv WMI class as inspired by mgmclassgen.exe.</summary>
-/// <version>0.0.1.0</version>
+/// <version>0.0.1.1</version>
 
 namespace ROOT.CIMV2
 
 open System
 open System.Diagnostics
-open System.Runtime.InteropServices
 open System.Reflection
-open WbemScripting
+open System.Management
 
 [<assembly: AssemblyTitle("StdRegProv")>]
 
@@ -22,198 +21,86 @@ module StdRegProv =
   let private GetMethodName (stackTrace: StackTrace) =
     stackTrace.GetFrame(0).GetMethod().Name
 
-  /// <summary>Release the specified COM object.</summary>
-  /// <param name="comObject">The COM object to destroy.</param>
-  let private ReleaseComObject<'a> (comObject: 'a byref) =
-    Marshal.FinalReleaseComObject comObject |> ignore
-    comObject <- Unchecked.defaultof<'a>
-
-  /// <summary>Set the specified property of a SWbemObject instance.</summary>
-  /// <param name="inParams">The object to set the property value from.</param>
-  /// <param name="propertyName">The property name.</param>
-  /// <param name="propertyValue">The property value.</param>
-  let private SetWBemObjectProperty (inParams: SWbemObject byref) (propertyName: string) (propertyValue) =
-    let mutable inProperties = inParams.Properties_
-    let mutable property = inProperties.[propertyName]
-    property.Value <- ref propertyValue
-    ReleaseComObject &property
-    ReleaseComObject &inProperties
-
-  /// <summary>Get the specified property of a SWbemObject instance.</summary>
-  /// <param name="outParams">The object to get the property value from.</param>
-  /// <param name="propertyName">The property name.</param>
-  /// <returns>The property value.</returns>
-  let private GetWBemObjectProperty<'a> (outParams: SWbemObject byref) (propertyName: string): 'a =
-    let mutable outProperties = outParams.Properties_
-    let mutable property = outProperties.Item(propertyName)
-    try
-      property.Value :?> 'a
-    finally
-      ReleaseComObject &property
-      ReleaseComObject &outProperties
-
-  /// <summary>Get the return value from the specified output parameter object.</summary>
-  /// <param name="outParams">The object to get the property value from.</param>
-  /// <returns>The property value.</returns>
-  let private GetReturnValue (outParams: SWbemObject byref) =
-    GetWBemObjectProperty<int> &outParams "ReturnValue"
-
-  type private StdRegInput (classObj: SWbemObject byref, methodName: string) =
-    let mutable wbemMethodSet = classObj.Methods_
-    let mutable wbemMethod = wbemMethodSet.[methodName]
-    let mutable inParamsClass = wbemMethod.InParameters
-    let mutable _params = inParamsClass.SpawnInstance_()
-    member val Params = _params with get
-
-    interface IDisposable with
-      member this.Dispose (): unit =
-        ReleaseComObject &_params
-        ReleaseComObject &inParamsClass
-        ReleaseComObject &wbemMethod
-        ReleaseComObject &wbemMethodSet
-
-  type private StdRegProvider () =
-    let mutable wbemLocator = new SWbemLocatorClass()
-    let mutable wbemService = wbemLocator.ConnectServer()
-    let mutable provider = wbemService.Get(CreatedClassName)
-    member val Provider = provider with get
-
-    interface IDisposable with
-      member this.Dispose (): unit =
-        ReleaseComObject &provider
-        ReleaseComObject &wbemService
-        ReleaseComObject &wbemLocator
-
-  let CheckAccess (hDefKey: uint, sSubKeyName: string, bGranted: bool byref) =
+  let CheckAccess (hDefKey: uint) (sSubKeyName: string) (bGranted: bool byref) =
     let methodName = GetMethodName (new StackTrace())
-    use Registry = new StdRegProvider()
-    let mutable classObj = Registry.Provider
-    use Input = new StdRegInput(&classObj, methodName)
-    let mutable inParams = Input.Params
-    SetWBemObjectProperty &inParams "hDefKey" (int hDefKey)
-    SetWBemObjectProperty &inParams "sSubKeyName" sSubKeyName
-    let mutable outParams = classObj.ExecMethod_(methodName, inParams)
-    bGranted <- GetWBemObjectProperty &outParams "bGranted"
-    try
-      GetReturnValue &outParams
-    finally
-      ReleaseComObject &inParams
-      ReleaseComObject &outParams
-      ReleaseComObject &classObj
+    let mutable classObj = new ManagementClass(CreatedClassName)
+    let mutable inParams = classObj.GetMethodParameters methodName
+    inParams.["hDefKey"] <- hDefKey
+    inParams.["sSubKeyName"] <- sSubKeyName
+    let mutable outParams = classObj.InvokeMethod(methodName, inParams, null)
+    bGranted <- outParams.["bGranted"] :?> bool
+    outParams.["ReturnValue"] :?> uint
 
-  let CreateKey (hDefKey: uint, sSubKeyName: string) =
+  let CreateKey (hDefKey: uint) (sSubKeyName: string) =
     let methodName = GetMethodName (new StackTrace())
-    use Registry = new StdRegProvider()
-    let mutable classObj = Registry.Provider
-    use Input = new StdRegInput(&classObj, methodName)
-    let mutable inParams = Input.Params
-    SetWBemObjectProperty &inParams "hDefKey" (int hDefKey)
-    SetWBemObjectProperty &inParams "sSubKeyName" sSubKeyName
-    let mutable outParams = classObj.ExecMethod_(methodName, inParams)
-    try
-      GetReturnValue &outParams
-    finally
-      ReleaseComObject &inParams
-      ReleaseComObject &outParams
-      ReleaseComObject &classObj
+    let mutable classObj = new ManagementClass(CreatedClassName)
+    let mutable inParams = classObj.GetMethodParameters methodName
+    inParams.["hDefKey"] <- hDefKey
+    inParams.["sSubKeyName"] <- sSubKeyName
+    let mutable outParams = classObj.InvokeMethod(methodName, inParams, null)
+    outParams.["ReturnValue"] :?> uint
 
-  let DeleteKey (hDefKey: uint, sSubKeyName: string) =
+  let DeleteKey (hDefKey: uint) (sSubKeyName: string) =
     let methodName = GetMethodName (new StackTrace())
-    use Registry = new StdRegProvider()
-    let mutable classObj = Registry.Provider
-    use Input = new StdRegInput(&classObj, methodName)
-    let mutable inParams = Input.Params
-    SetWBemObjectProperty &inParams "hDefKey" (int hDefKey)
-    SetWBemObjectProperty &inParams "sSubKeyName" sSubKeyName
-    let mutable outParams = classObj.ExecMethod_(methodName, inParams)
-    try
-      GetReturnValue &outParams
-    finally
-      ReleaseComObject &inParams
-      ReleaseComObject &outParams
-      ReleaseComObject &classObj
+    let mutable classObj = new ManagementClass(CreatedClassName)
+    let mutable inParams = classObj.GetMethodParameters methodName
+    inParams.["hDefKey"] <- hDefKey
+    inParams.["sSubKeyName"] <- sSubKeyName
+    let mutable outParams = classObj.InvokeMethod(methodName, inParams, null)
+    outParams.["ReturnValue"] :?> uint
 
-  let DeleteValue (hDefKey: uint, sSubKeyName: string, sValueName: string | null) =
+  let DeleteValue (hDefKey: uint) (sSubKeyName: string) (sValueName: string | null) =
     let methodName = GetMethodName (new StackTrace())
-    use Registry = new StdRegProvider()
-    let mutable classObj = Registry.Provider
-    use Input = new StdRegInput(&classObj, methodName)
-    let mutable inParams = Input.Params
-    SetWBemObjectProperty &inParams "hDefKey" (int hDefKey)
-    SetWBemObjectProperty &inParams "sSubKeyName" sSubKeyName
-    SetWBemObjectProperty &inParams "sValueName" sValueName
-    let mutable outParams = classObj.ExecMethod_(methodName, inParams)
-    try
-      GetReturnValue &outParams
-    finally
-      ReleaseComObject &inParams
-      ReleaseComObject &outParams
-      ReleaseComObject &classObj
+    let mutable classObj = new ManagementClass(CreatedClassName)
+    let mutable inParams = classObj.GetMethodParameters methodName
+    inParams.["hDefKey"] <- hDefKey
+    inParams.["sSubKeyName"] <- sSubKeyName
+    inParams.["sValueName"] <- sValueName
+    let mutable outParams = classObj.InvokeMethod(methodName, inParams, null)
+    outParams.["ReturnValue"] :?> uint
 
-  let EnumKey (hDefKey: uint, sSubKeyName: string, sNames: string array byref) =
+  let EnumKey (hDefKey: uint) (sSubKeyName: string) (sNames: string array byref) =
     let methodName = GetMethodName (new StackTrace())
-    use Registry = new StdRegProvider()
-    let mutable classObj = Registry.Provider
-    use Input = new StdRegInput(&classObj, methodName)
-    let mutable inParams = Input.Params
-    SetWBemObjectProperty &inParams "hDefKey" (int hDefKey)
-    SetWBemObjectProperty &inParams "sSubKeyName" sSubKeyName
-    let mutable outParams = classObj.ExecMethod_(methodName, inParams)
-    try
-      let sNamesObj = GetWBemObjectProperty<obj> &outParams "sNames"
-      sNames <-
-        match sNamesObj with
-        | :? DBNull -> Array.Empty()
-        | :? array<obj> as names -> Array.map (fun name -> name.ToString()) names
-        | _ -> [||]
-      GetReturnValue &outParams
-    finally
-      ReleaseComObject &inParams
-      ReleaseComObject &outParams
-      ReleaseComObject &classObj
+    let mutable classObj = new ManagementClass(CreatedClassName)
+    let mutable inParams = classObj.GetMethodParameters methodName
+    inParams.["hDefKey"] <- hDefKey
+    inParams.["sSubKeyName"] <- sSubKeyName
+    let mutable outParams = classObj.InvokeMethod(methodName, inParams, null)
+    let sNamesObj = outParams.["sNames"]
+    sNames <-
+      match sNamesObj with
+      | :? array<obj> as names -> Array.map (fun name -> name.ToString()) names
+      | _ -> [||]
+    outParams.["ReturnValue"] :?> uint
 
-  let GetStringValue (hDefKey: uint Nullable, sSubKeyName: string, sValueName: string | null, sValue: string byref) =
+  let GetStringValue (hDefKey: uint Nullable) (sSubKeyName: string) (sValueName: string | null) (sValue: string byref) =
     let methodName = GetMethodName (new StackTrace())
-    use Registry = new StdRegProvider()
-    let mutable classObj = Registry.Provider
-    use Input = new StdRegInput(&classObj, methodName)
-    let mutable inParams = Input.Params
-    if hDefKey.HasValue then SetWBemObjectProperty &inParams "hDefKey" (int hDefKey.Value)
-    SetWBemObjectProperty &inParams "sSubKeyName" sSubKeyName
-    SetWBemObjectProperty &inParams "sValueName" sValueName
-    let mutable outParams = classObj.ExecMethod_(methodName, inParams)
-    sValue <- GetWBemObjectProperty &outParams "sValue"
-    try
-      GetReturnValue &outParams
-    finally
-      ReleaseComObject &inParams
-      ReleaseComObject &outParams
-      ReleaseComObject &classObj
+    let mutable classObj = new ManagementClass(CreatedClassName)
+    let mutable inParams = classObj.GetMethodParameters methodName
+    if hDefKey.HasValue then inParams.["hDefKey"] <- (hDefKey.Value)
+    inParams.["sSubKeyName"] <- sSubKeyName
+    inParams.["sValueName"] <- sValueName
+    let mutable outParams = classObj.InvokeMethod(methodName, inParams, null)
+    sValue <- outParams.["sValue"] :?> string
+    outParams.["ReturnValue"] :?> uint
 
-  let SetStringValue (hDefKey: uint, sSubKeyName: string, sValueName: string | null, sValue: string | null) =
+  let SetStringValue (hDefKey: uint) (sSubKeyName: string) (sValueName: string | null) (sValue: string | null) =
     let methodName = GetMethodName (new StackTrace())
-    use Registry = new StdRegProvider()
-    let mutable classObj = Registry.Provider
-    use Input = new StdRegInput(&classObj, methodName)
-    let mutable inParams = Input.Params
-    SetWBemObjectProperty &inParams "hDefKey" (int hDefKey)
-    SetWBemObjectProperty &inParams "sSubKeyName" sSubKeyName
-    SetWBemObjectProperty &inParams "sValueName" sValueName
-    SetWBemObjectProperty &inParams "sValue" sValue
-    let mutable outParams = classObj.ExecMethod_(methodName, inParams)
-    try
-      GetReturnValue &outParams
-    finally
-      ReleaseComObject &inParams
-      ReleaseComObject &outParams
-      ReleaseComObject &classObj
+    let mutable classObj = new ManagementClass(CreatedClassName)
+    let mutable inParams = classObj.GetMethodParameters methodName
+    inParams.["hDefKey"] <- hDefKey
+    inParams.["sSubKeyName"] <- sSubKeyName
+    inParams.["sValueName"] <- sValueName
+    inParams.["sValue"] <- sValue
+    let mutable outParams = classObj.InvokeMethod(methodName, inParams, null)
+    outParams.["ReturnValue"] :?> uint
 
   /// <summary>Remove the key and all descendant subkeys.</summary>
-  let rec DeleteKeyTree (hDefKey, sSubKeyName) =
+  let rec DeleteKeyTree hDefKey sSubKeyName =
     let mutable sNames = Array.Empty()
-    let mutable returnValue = EnumKey(hDefKey, sSubKeyName, &sNames)
+    let mutable returnValue = EnumKey hDefKey sSubKeyName &sNames
     if sNames.Length > 0 then
       for sName in sNames do
-        returnValue <- returnValue + DeleteKeyTree (hDefKey, $"{sSubKeyName}\\{sName}")
-    returnValue <- returnValue + DeleteKey(hDefKey, sSubKeyName)
+        returnValue <- returnValue + DeleteKeyTree hDefKey $"{sSubKeyName}\\{sName}"
+    returnValue <- returnValue + DeleteKey hDefKey sSubKeyName
     returnValue
